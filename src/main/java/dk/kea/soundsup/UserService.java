c8daf94977c84535b601e72e4f087e5dd6de241e
AndreiAta <andrei.atanasiu1994@gmail.com>
2017-03-27 12:33:15 +0200

    Added POST method for Google SignIn in the API

--- src/main/java/dk/kea/soundsup/UserService.java
+++ src/main/java/dk/kea/soundsup/UserService.java
@@ -1,23 +1,79 @@
 package dk.kea.soundsup;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.LowLevelHttpRequest;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonGenerator;
+import com.google.api.client.json.JsonParser;
 import dk.kea.soundsup.model.User;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by mancr on 3/6/2017.
  */
 @Path("/users")
-public class UserService {
+public class UserService
+{
+    HttpTransport transport = new HttpTransport()
+    {
+        @Override
+        protected LowLevelHttpRequest buildRequest(String s, String s1) throws IOException
+        {
+            return null;
+        }
+    };
+
+    JsonFactory jsonFactory = new JsonFactory()
+    {
+        public JsonParser createJsonParser(InputStream inputStream) throws IOException
+        {
+            return null;
+        }
+
+        public JsonParser createJsonParser(InputStream inputStream, Charset charset) throws IOException
+        {
+            return null;
+        }
+
+        public JsonParser createJsonParser(String s) throws IOException
+        {
+            return null;
+        }
+
+        public JsonParser createJsonParser(Reader reader) throws IOException
+        {
+            return null;
+        }
+
+        public JsonGenerator createJsonGenerator(OutputStream outputStream, Charset charset) throws IOException
+        {
+            return null;
+        }
+
+        public JsonGenerator createJsonGenerator(Writer writer) throws IOException
+        {
+            return null;
+        }
+    };
+
+    GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+            .setAudience(Collections.singletonList("176022414732-multinga95b6se0j9024vogb8t24rvge.apps.googleusercontent.com"))
+            .build();
+
     public List<User> getAllUsersDB() {
         List<User> users = new ArrayList<User>();
 
@@ -48,5 +104,38 @@ public class UserService {
         List<User> users = getAllUsersDB();
 
         return users.get(id);
+    }
+
+    @POST
+    @Path("/{param}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getGoogleId(@PathParam("param") String idTokenString) throws GeneralSecurityException, IOException
+    {
+        System.out.println("inside post method . .");
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+            // Use or store profile information
+            System.out.println("The id is good :D");
+
+        } else {
+            System.out.println("Invalid ID token.");
+        }
+
+        return Response.status(201).entity(idTokenString).build();
     }
 }
