8a263d33fbf1e13a55dc3189769a07bd94c7e79f
AndreiAta <andrei.atanasiu1994@gmail.com>
2017-05-08 09:58:48 +0200

    Added the CORSResponseFilter as a param to web.xml and added a new SoundsUp class to be used as a ResourceConfig

---
+++ src/main/java/dk/kea/soundsup/SoundsUp.java
@@ -0,0 +1,30 @@
+package dk.kea.soundsup;
+
+
+import com.sun.jersey.api.core.ResourceConfig;
+
+import java.util.Map;
+
+public class SoundsUp extends ResourceConfig
+{
+    public Map<String, Boolean> getFeatures()
+    {
+        return null;
+    }
+
+    public boolean getFeature(String s)
+    {
+        return false;
+    }
+
+    public Map<String, Object> getProperties()
+    {
+        return null;
+    }
+
+    public Object getProperty(String s)
+    {
+        return null;
+    }
+
+}
