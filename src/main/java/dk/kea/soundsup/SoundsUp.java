f18dd9c5c5ff1cdc9764d1e437482c814d3557f8
AndreiAta <andreww0007@gmail.com>
2017-05-15 13:17:39 +0200

    Trying to add main entry point to app for Heroku

--- src/main/java/dk/kea/soundsup/SoundsUp.java
+++ src/main/java/dk/kea/soundsup/SoundsUp.java
@@ -1,20 +1,26 @@
 package dk.kea.soundsup;
 
+import com.sun.jersey.api.core.ResourceConfig;
+import dk.kea.soundsup.utility.CORSResponseFilter;
+
 import javax.ws.rs.core.Application;
+import java.io.File;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class SoundsUp extends Application
+public class SoundsUp extends org.glassfish.jersey.server.ResourceConfig
 {
-    @Override
-    public Set<Class<?>> getClasses() {
-        final Set<Class<?>> classes = new HashSet<Class<?>>();
+    /**
+     * Register JAX-RS application components.
+     */
+    public SoundsUp()
+    {
+        //register application resources
+        register(UserService.class);
+        register(HelloWorld.class);
 
-        // register resources and features
-        classes.add(UserService.class);
-        classes.add(HelloWorld.class);
-
-        return classes;
+        //register filters
+        register(CORSResponseFilter.class);
     }
 }
