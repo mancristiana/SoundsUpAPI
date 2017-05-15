aa2a63e43f98cebec6c7e22c18c1a5904de6b382
AndreiAta <andreww0007@gmail.com>
2017-05-15 13:53:51 +0200

    Added try and catch block to SoundsUp because Heroku couldnt recognize GlassFish exception

--- src/main/java/dk/kea/soundsup/SoundsUp.java
+++ src/main/java/dk/kea/soundsup/SoundsUp.java
@@ -7,14 +7,22 @@ import java.io.File;
 
 public class SoundsUp
 {
-    public static void main(String[] args) throws GlassFishException {
+    public static void main(String[] args)
+    {
         String port = System.getenv("PORT");
         GlassFishProperties gfProps = new GlassFishProperties();
         gfProps.setPort("http-listener", Integer.parseInt(port));
-        GlassFish glassFish = GlassFishRuntime.bootstrap().newGlassFish(gfProps);
-        glassFish.start();
-        Deployer deployer = glassFish.getDeployer();
-        File file = new File("SoundsUpApi.war");
-        deployer.deploy(file);
+        try
+        {
+            GlassFish glassFish = GlassFishRuntime.bootstrap().newGlassFish(gfProps);
+            glassFish.start();
+            Deployer deployer = glassFish.getDeployer();
+            File file = new File("SoundsUpApi.war");
+            deployer.deploy(file);
+        }catch(Exception exc)
+        {
+            exc.printStackTrace();
+        }
+
     }
 }
