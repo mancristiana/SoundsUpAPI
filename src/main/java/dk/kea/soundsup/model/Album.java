43929d358888b520d1b04f998922b8321e149831
Cristiana Man <man.cristiana1@gmail.com>
2017-05-18 00:51:08 +0200

    Fixed album image url not showing

--- src/main/java/dk/kea/soundsup/model/Album.java
+++ src/main/java/dk/kea/soundsup/model/Album.java
@@ -32,5 +32,11 @@ public class Album {
         this.name = name;
     }
 
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
