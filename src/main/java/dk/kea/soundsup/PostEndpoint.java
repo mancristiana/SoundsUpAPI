3d62e0812092178ac8be8a630023257ad7c88563
Cristiana Man <man.cristiana1@gmail.com>
2017-05-17 12:01:18 +0200

    Created a basic post endpoint

---
+++ src/main/java/dk/kea/soundsup/PostEndpoint.java
@@ -0,0 +1,7 @@
+package dk.kea.soundsup;
+
+/**
+ * Created by mancr on 17-May-17.
+ */
+public class PostEndpoint {
+}
---
+++ src/main/java/dk/kea/soundsup/model/Post.java
@@ -0,0 +1,7 @@
+package dk.kea.soundsup.model;
+
+/**
+ * Created by mancr on 17-May-17.
+ */
+public class Post {
+}
