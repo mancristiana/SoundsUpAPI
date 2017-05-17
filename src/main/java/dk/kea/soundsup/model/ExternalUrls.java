c1c21e98c05ae127f1dad7fc00be95d0f4394971
Cristiana Man <man.cristiana1@gmail.com>
2017-05-18 00:35:22 +0200

    Prepared models for more advanced storage of post object

---
+++ src/main/java/dk/kea/soundsup/model/ExternalUrls.java
@@ -0,0 +1,20 @@
+package dk.kea.soundsup.model;
+
+/**
+ * Created by mancr on 17-May-17.
+ */
+public class ExternalUrls {
+    private String spotify;
+
+    public ExternalUrls(String spotify) {
+        this.spotify = spotify;
+    }
+
+    public String getSpotify() {
+        return spotify;
+    }
+
+    public void setSpotify(String spotify) {
+        this.spotify = spotify;
+    }
+}
