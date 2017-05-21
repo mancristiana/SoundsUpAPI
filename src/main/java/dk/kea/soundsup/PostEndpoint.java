856756f33713ed6cd9a5bcbbd8636146aea0339f
Cristiana Man <man.cristiana1@gmail.com>
2017-05-21 20:48:18 +0200

    Updated insert post endpoint

--- src/main/java/dk/kea/soundsup/PostEndpoint.java
+++ src/main/java/dk/kea/soundsup/PostEndpoint.java
@@ -1,16 +1,13 @@
 package dk.kea.soundsup;
 
 import dk.kea.soundsup.db.PostDAO;
+import dk.kea.soundsup.db.TrackDAO;
 import dk.kea.soundsup.model.Post;
-import dk.kea.soundsup.model.User;
+import dk.kea.soundsup.model.ResponseMessage;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.*;
 
 /**
  * Created by mancr on 17-May-17.
@@ -29,36 +26,34 @@ public class PostEndpoint {
      * @apiSuccess {Number} user_id Unique user identifier generated on sign up.
      * @apiSuccess {String} track_id Unique identifier for the track corresponding to Spotify API Database.
      * @apiSuccess {String} description Content of the post
-
      * @apiSuccessExample Success Response
      * HTTP/1.1 200 OK
      * [
-     *    {
-     *        "postId": 0,
-     *        "userId": 23,
-     *        "trackId": "2eOn7OyFJ8ygzBrWGirpsB",
-     *        "description": "Some song"
-     *    },
-     *    {
-     *        "postId": 1,
-     *        "userId": 32,
-     *        "trackId": "1e2GBOtPyLu7iuNm4EvFKG",
-     *        "description": "Another description"
-     *    },
-     *    {
-     *        "postId": 2,
-     *        "userId": 11,
-     *        "trackId": "1zRFuPnqjBckEoOFliJyaI",
-     *        "description": "Such wow"
-     *    },
-     *    {
-     *        "postId": 3,
-     *        "userId": 11,
-     *        "trackId": "27HNh1cyB39ERqdpSjM2i1",
-     *        "description": "Amazing"
-     *    }
-     *]
-     *
+     * {
+     * "postId": 0,
+     * "userId": 23,
+     * "trackId": "2eOn7OyFJ8ygzBrWGirpsB",
+     * "description": "Some song"
+     * },
+     * {
+     * "postId": 1,
+     * "userId": 32,
+     * "trackId": "1e2GBOtPyLu7iuNm4EvFKG",
+     * "description": "Another description"
+     * },
+     * {
+     * "postId": 2,
+     * "userId": 11,
+     * "trackId": "1zRFuPnqjBckEoOFliJyaI",
+     * "description": "Such wow"
+     * },
+     * {
+     * "postId": 3,
+     * "userId": 11,
+     * "trackId": "27HNh1cyB39ERqdpSjM2i1",
+     * "description": "Amazing"
+     * }
+     * ]
      * @apiError (Error 4xx) 400 Bad request
      * @apiError (Error 5xx) 500 Server has encountered a problem
      */
@@ -83,12 +78,11 @@ public class PostEndpoint {
      * @apiSuccess {String} description Content of the post
      * @apiSuccessExample Success-Response:
      * {
-     *     "post_id": 3,
-     *     "user_id": 1,
-     *     "track_id": "someIdb436427463mnj4n6k42l",
-     *     "description": "OMG This song is amazing!"
+     * "post_id": 3,
+     * "user_id": 1,
+     * "track_id": "someIdb436427463mnj4n6k42l",
+     * "description": "OMG This song is amazing!"
      * }
-     *
      * @apiError (Error 4xx) 400 Bad request
      * @apiError (Error 4xx) 404 Post not found
      * @apiError (Error 5xx) 500 Server has encountered a problem
@@ -99,7 +93,31 @@ public class PostEndpoint {
     @Produces({MediaType.APPLICATION_JSON})
     public Post getPostById(@PathParam("id") int id) {
         List<Post> posts = getAllPosts();
-        return posts.get(id);
+        return posts.get(id); // TODO CORRECT IMPLEMENTATION
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response createPost(Post post) {
+        ResponseMessage responseMessage = new ResponseMessage();
+
+        int trackId = TrackDAO.insertTrack(post.getTrack());
+        post.getTrack().setId(trackId);
+        int postId = PostDAO.insertPost(post);
+        if (trackId == -1 || postId == -1) {
+            responseMessage.setStatus(400);
+            responseMessage.setMessage("An error occured while trying to process your request");
+        } else {
+            responseMessage.setStatus(200);
+            responseMessage.setMessage("Post with id = " + postId + " was sucessfully inserted.");
+        }
+
+
+        return Response
+                .status(responseMessage.getStatus())
+                .entity(responseMessage)
+                .build();
     }
 
 }
