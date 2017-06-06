f30ed4dff5e30aa6fa20e102afd095975c494b6e
Cristiana Man <man.cristiana1@gmail.com>
2017-06-06 13:28:55 +0200

    Updated docs

--- src/main/java/dk/kea/soundsup/resources/SpotifyResource.java
+++ src/main/java/dk/kea/soundsup/resources/SpotifyResource.java
@@ -15,6 +15,22 @@ import java.io.IOException;
 @Path("/spotify")
 public class SpotifyResource {
 
+    /**
+     * @api {get} /spotify/token Get Spotify access token
+     * @apiName GetSpotifyToken
+     * @apiGroup Spotify
+     * @apiVersion 1.0.0
+     * @apiDescription This request returns a token needed during the client credentials flow to access resources from the Spotify Web API.
+     * @apiSuccess {String} idToken token allowing you to make requests to the Spotify Web API endpoints that do not require user authorization. For example, Get a track endpoint.
+     * @apiSuccessExample {json} SuccessResponse
+     * HTTP/1.1 200 OK
+     * {
+     *      "idToken": "BQB4eNFgjk2oxbYdNz0gMd58eMRw7k5wddbn5dA2oXt570uzVf3oK1ozrCYyB_kVkZynJxqoB-VHIdLbqkIz_w"
+     * }
+     *
+     * @apiError (Error 4xx) 400 Bad request
+     * @apiError (Error 5xx) 500 Server has encountered a problem
+     */
 
     @GET
     @Path("/token")
